b185dd418ca28c05e86b9fd1a74d6ad1717332f2
FawenYo <sky80838@gmail.com>
2023-04-22 21:08:36 +0800

    Add start_time and end_time

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from utils.game import Game
 
 
 def main():
+    start_time = 1673308800  # 2023/01/10 00:00:00
+    end_time = 1682193600  # 2023/04/22 20:00:00
     game = Game()
-    games = game.fetch_all()
+    games = game.fetch_all(start_time=start_time, end_time=end_time)
     pickle.dump(games, open("games.pkl", "wb"))
 
 
--- utils/game.py
+++ utils/game.py
@@ -42,18 +42,26 @@ class Game:
         player.summoner_level = info["summonerLevel"]
         return player
 
-    def get_match_id(self, user_puuid: str, count: int) -> list[str]:
+    def get_match_id(
+        self, user_puuid: str, start_time: int, end_time: int, count: int
+    ) -> list[str]:
         """Get match id
 
         Args:
             user_puuid (str): Player puuid
+            start_time (int): Start time
+            end_time (int): End time
             count (int): Number of matches
 
         Returns:
             list[str]: List of match id
         """
         return self.lol_watcher.match.matchlist_by_puuid(
-            region=self.server_region, puuid=user_puuid, count=count
+            region=self.server_region,
+            puuid=user_puuid,
+            count=count,
+            start_time=start_time,
+            end_time=end_time,
         )
 
     def get_match_info(self, match_id: str) -> dict:
@@ -158,13 +166,19 @@ class Game:
         else:
             return pickle.load(open("players.pkl", "rb"))
 
-    def get_all_maych_id(
-        self, players: list[Player], new_data: bool = True
+    def get_all_match_id(
+        self,
+        players: list[Player],
+        start_time: int,
+        end_time: int,
+        new_data: bool = True,
     ) -> list[str]:
         """Get all match id
 
         Args:
             players (list[Player]): List of players
+            start_time (int): Start time
+            end_time (int): End time
             new_data (bool, optional): Get new data or not. Defaults to True.
 
         Returns:
@@ -174,7 +188,12 @@ class Game:
             match_id_list = []
             for player in tqdm(players):
                 match_id_list.append(
-                    self.get_match_id(user_puuid=player.puuid, count=100)
+                    self.get_match_id(
+                        user_puuid=player.puuid,
+                        start_time=start_time,
+                        end_time=end_time,
+                        count=100,
+                    )
                 )
             pickle.dump(match_id_list, open("match_id_list.pkl", "wb"))
             return match_id_list
@@ -193,8 +212,12 @@ class Game:
         match_info = MatchInfo(data=match_info)
         return match_info
 
-    def fetch_all(self) -> list[str]:
+    def fetch_all(self, start_time: int, end_time: int) -> list[str]:
         """Fetch all data
+
+        Args:
+            start_time (int): Start time
+            end_time (int): End time
 
         Returns:
             list[str]: List of match info
@@ -204,7 +227,9 @@ class Game:
         players = self.get_all_players()
 
         # Get match list
-        match_id_list = self.get_all_maych_id(players=players)
+        match_id_list = self.get_all_match_id(
+            players=players, start_time=start_time, end_time=end_time
+        )
         for match in tqdm(match_id_list):
             match_info_dict = self.get_match_info(match_id=match)
             match_info = self.parse_match_info(match_info=match_info_dict)
