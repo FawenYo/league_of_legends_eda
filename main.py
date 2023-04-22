82bf284c1b2cc5206ba64abf9dec7dc760dfe62c
FawenYo <sky80838@gmail.com>
2023-04-22 20:47:55 +0800

    Save games into `pkl`

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import pickle
+
 from utils.game import Game
 
 
 def main():
     game = Game()
     games = game.fetch_all()
+    pickle.dump(games, open("games.pkl", "wb"))
 
 
 if __name__ == "__main__":
